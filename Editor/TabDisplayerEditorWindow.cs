ed8353f17240e87173c767f65387d3583a76ab88
Shariar Papon <spaponwork@gmail.com>
2023-02-24 09:13:42 -0500

    utility functions added

--- Editor/TabDisplayerEditorWindow.cs
+++ Editor/TabDisplayerEditorWindow.cs
@@ -2,134 +2,158 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 
-public abstract class TabDisplayerEditorWindow : EditorWindow
+namespace Everime.CustomEditor
 {
-    private const float BUTTON_SECTION_WIDTH = 180;
-    private const float BUTTON_SECTION_TOP_OFFSET = 4;
-    private readonly Color BUTTON_SECTION_COLOR = new(147f / 255, 160f / 255, 166f / 255);
-    private readonly Color TAB_DISPLAY_SECTION_COLOR = new(91f / 255, 100f / 255, 110f / 255);
-
-    private Tab ActiveTab = null;
-    private List<Tab> allTabs;
-
-    private void OnEnable()
+    public abstract class TabDisplayerEditorWindow : EditorWindow
     {
-        OnWindowInitialize();
-    }
+        private const float BUTTON_SECTION_WIDTH = 180;
+        private const float BUTTON_SECTION_TOP_OFFSET = 4;
+        protected Color buttonSectionColor = new(147f / 255, 160f / 255, 166f / 255);
+        protected Color tabDisplaySectionColor = new(91f / 255, 100f / 255, 110f / 255);
 
-    private void OnGUI()
-    {
-        GUILayout.BeginHorizontal();
+        private Tab ActiveTab = null;
+        private List<Tab> allTabs;
 
-        EditorGUI.DrawRect(new Rect(0, 0, BUTTON_SECTION_WIDTH, position.height), BUTTON_SECTION_COLOR);
-        DrawButtonSection();
-
-        EditorGUI.DrawRect(new Rect(BUTTON_SECTION_WIDTH, 0, position.width - BUTTON_SECTION_WIDTH, position.height), TAB_DISPLAY_SECTION_COLOR);
-        DrawTabDisplaySection();
-
-        GUILayout.EndHorizontal();
-    }
-
-    protected virtual void OnWindowInitialize()
-    {
-        SetWindowTitle("Tab Window");
-    }
-
-    protected virtual void DrawButtonSection()
-    {
-        GUILayout.BeginVertical(GUILayout.Width(BUTTON_SECTION_WIDTH));
-        GUILayout.Space(BUTTON_SECTION_TOP_OFFSET);
-        if (allTabs != null)
+        private void OnEnable()
         {
-            for (int i = 0; i < allTabs.Count; i++)
+            OnWindowInitialize();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+
+            EditorGUI.DrawRect(new Rect(0, 0, BUTTON_SECTION_WIDTH, position.height), buttonSectionColor);
+            DrawButtonSection();
+
+            EditorGUI.DrawRect(new Rect(BUTTON_SECTION_WIDTH, 0, position.width - BUTTON_SECTION_WIDTH, position.height), tabDisplaySectionColor);
+            DrawTabDisplaySection();
+
+            GUILayout.EndHorizontal();
+        }
+
+        protected virtual void OnWindowInitialize()
+        {
+            Debug.Log("Tab window intialized");
+        }
+
+        protected virtual void DrawButtonSection()
+        {
+            GUILayout.BeginVertical(GUILayout.Width(BUTTON_SECTION_WIDTH));
+            GUILayout.Space(BUTTON_SECTION_TOP_OFFSET);
+            if (allTabs != null)
             {
-                if (GUILayout.Button(allTabs[i].name, GUILayout.Width(BUTTON_SECTION_WIDTH - 8)))
+                for (int i = 0; i < allTabs.Count; i++)
                 {
-                    ActiveTab = allTabs[i];
+                    if (DrawTabButton(allTabs[i].name))
+                    {
+                        ActiveTab = allTabs[i];
+                    }
+                    GUILayout.Space(2);
                 }
-                GUILayout.Space(2);
             }
+            GUILayout.EndVertical();
         }
-        GUILayout.EndVertical();
-    }
 
-    protected virtual void DrawTabDisplaySection()
-    {
-        GUILayout.BeginVertical();
-        ActiveTab?.drawer.Invoke();
-        GUILayout.EndVertical();
-    }
-
-    public void SetDefaultTab(int index)
-    {
-	if(allTabs != null)
-	{
-        	index %= allTabs.Count;
-        	if (index < 0 || allTabs.Count <= 0)
-        	{
-            		return;
-        	}
-        	ActiveTab = allTabs[index];
-	}
-    }
-
-    public void SetDefaultTab(Tab tab)
-    {
-        if (tab != null && allTabs != null)
+        protected virtual void DrawTabDisplaySection()
         {
-            if (!allTabs.Contains(tab))
+            GUILayout.BeginVertical();
+            ActiveTab?.drawer.Invoke();
+            GUILayout.EndVertical();
+        }
+
+        public void SetDefaultTab(int index)
+        {
+            if (allTabs != null)
             {
-                allTabs.Add(tab);
+                index %= allTabs.Count;
+                if (index < 0 || allTabs.Count <= 0)
+                {
+                    return;
+                }
+                ActiveTab = allTabs[index];
             }
-            ActiveTab = tab;
         }
-    }
 
-    public void NewTab(Tab tab)
-    {
-	if(tab == null) return;
-
-        if (allTabs == null)
+        public bool DrawTabButton(string name)
         {
-            allTabs = new List<Tab>();
+            return GUILayout.Button(name, GUILayout.Width(BUTTON_SECTION_WIDTH - 8));
         }
-        allTabs.Add(tab);
-    }
 
-    public Tab NewTab(string name, System.Action drawer)
-    {
-        Tab tab = new(name, drawer);
-        NewTab(tab);
-        return tab;
-    }
-
-    public void NewTabs(params Tab[] tabs)
-    {
-        foreach (Tab tab in tabs)
+        public void SetDefaultTab(Tab tab)
         {
+            if (tab != null && allTabs != null)
+            {
+                if (!allTabs.Contains(tab))
+                {
+                    allTabs.Add(tab);
+                }
+                ActiveTab = tab;
+            }
+        }
+
+        public void NewTab(Tab tab)
+        {
+            if (tab == null) return;
+
+            if (allTabs == null)
+            {
+                allTabs = new List<Tab>();
+            }
+            if (allTabs.Contains(tab) == false)
+                allTabs.Add(tab);
+        }
+
+        public Tab NewTab(string name, System.Action drawer)
+        {
+            Tab tab = new(name, drawer);
             NewTab(tab);
+            return tab;
         }
-    }
 
-    public void ClearTabs()
-    {
-        allTabs = null;
-    }
-
-    public void SetWindowTitle(string title)
-    {
-        titleContent = new GUIContent(title);
-    }
-
-    public sealed class Tab
-    {
-        public readonly string name;
-        public readonly System.Action drawer;
-
-        public Tab(string name, System.Action drawer)
+        public void NewTabs(params Tab[] tabs)
         {
-            this.name = name;
-            this.drawer = drawer;
+            foreach (Tab tab in tabs)
+            {
+                NewTab(tab);
+            }
+        }
+
+        public void ClearTabs()
+        {
+            allTabs = null;
+        }
+
+        public void SetWindowTitle(string title)
+        {
+            titleContent = new GUIContent(title);
+        }
+
+        public void SetTabDisplaySectionColor(Color color)
+        {
+            tabDisplaySectionColor = color;
+        }
+
+        public void SetButtonSectionColor(Color color)
+        {
+            buttonSectionColor = color;
+        }
+
+        public void SetContentColor(Color color)
+        {
+            GUI.contentColor = color;
+        }
+
+        public sealed class Tab
+        {
+            public readonly string name;
+            public readonly System.Action drawer;
+
+            public Tab(string name, System.Action drawer)
+            {
+                this.name = name;
+                this.drawer = drawer;
+            }
         }
     }
 }
